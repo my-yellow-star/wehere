e84a389a1edef6f2afa08b932a1ed1852c5e45df
triphero-chan <c01034784796@gmail.com>
2022-11-28 14:45:04 +0900

    test: save member

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,13 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.liquibase:liquibase-core")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/api/epilogue/wehere/member/repository/MemberRepositoryTests.kt
@@ -0,0 +1,23 @@
+package api.epilogue.wehere.member.repository
+
+import api.epilogue.wehere.member.domain.Member
+import api.epilogue.wehere.member.domain.MemberRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class MemberRepositoryTests @Autowired constructor(
+    private val memberRepository: MemberRepository
+) {
+    @Test
+    @DisplayName("멤버 저장")
+    fun save() {
+        val member = Member("member_123")
+        val saved = memberRepository.save(member)
+        Assertions.assertEquals(saved.state, Member.MemberState.ACTIVE)
+        Assertions.assertEquals(saved.grade, Member.MemberGrade.FREE_TIER)
+    }
+}
