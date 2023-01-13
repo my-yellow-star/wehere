16777e563350b213250940e19f709c7f5b1af67c
triphero-chan <c01034784796@gmail.com>
2023-01-13 17:43:08 +0900

    style: securityConfig - inject dependency by general type

--- src/main/kotlin/api/epilogue/wehere/config/SecurityConfig.kt
+++ src/main/kotlin/api/epilogue/wehere/config/SecurityConfig.kt
@@ -1,21 +1,23 @@
 package api.epilogue.wehere.config
 
-import api.epilogue.wehere.auth.application.OAuth2UserServiceImpl
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
+import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig(
-    private val oAuth2UserServiceImpl: OAuth2UserServiceImpl
+    private val oAuth2UserService: OAuth2UserService<OAuth2UserRequest, OAuth2User>
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http
             .csrf().disable()
             .oauth2Login {
-                it.userInfoEndpoint().userService(oAuth2UserServiceImpl)
+                it.userInfoEndpoint().userService(oAuth2UserService)
                 it.defaultSuccessUrl("/api/auth/token")
             }
             .build()
