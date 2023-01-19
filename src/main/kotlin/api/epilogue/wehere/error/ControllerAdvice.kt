de59421605c7453dba0f7e5a66cb02f13aa18058
wehere-chan <c01034784796@gmail.com>
2023-01-19 15:17:04 +0900

    fix: custom error response

--- src/main/kotlin/api/epilogue/wehere/error/ControllerAdvice.kt
+++ src/main/kotlin/api/epilogue/wehere/error/ControllerAdvice.kt
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ControllerAdvice {
     @ExceptionHandler(value = [ApiError::class])
     fun handle(e: ApiError) =
-        ResponseEntity(ErrorResponse(e.message), e.errorCause.status)
+        ResponseEntity(ErrorResponse(e.errorCause.name, e.message), e.errorCause.status)
 
     data class ErrorResponse(
+        val type: String,
         val reason: String?
     )
 }
