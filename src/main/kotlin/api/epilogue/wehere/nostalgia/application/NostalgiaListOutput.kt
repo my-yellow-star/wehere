1fd0b0646c4ff8c7276e4881f6924868954f5a0c
wehere-chan <c01034784796@gmail.com>
2023-01-27 23:11:25 +0900

    fix: add title, description to NostalgiaListOutput

--- src/main/kotlin/api/epilogue/wehere/nostalgia/application/NostalgiaListOutput.kt
+++ src/main/kotlin/api/epilogue/wehere/nostalgia/application/NostalgiaListOutput.kt
@@ -8,6 +8,8 @@ import java.util.UUID
 data class NostalgiaListOutput(
     val id: UUID,
     val member: MemberListOutput,
+    val title: String,
+    val description: String,
     val location: Location,
     val distance: Int?,
     val thumbnail: String?,
@@ -17,6 +19,8 @@ data class NostalgiaListOutput(
         fun of(nostalgia: Nostalgia, current: Location?) = NostalgiaListOutput(
             id = nostalgia.id,
             member = MemberListOutput.of(nostalgia.member),
+            title = nostalgia.title,
+            description = nostalgia.description,
             location = Location.of(nostalgia.location),
             distance = current?.let { Location.of(nostalgia.location).distance(it).toInt() },
             thumbnail = nostalgia.thumbnailUrl,
