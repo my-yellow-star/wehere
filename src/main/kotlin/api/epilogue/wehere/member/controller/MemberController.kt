01844253f353c879cbdd3c44319b417b0ecfd540
wehere-chan <c01034784796@gmail.com>
2023-02-08 00:39:26 +0900

    feat: get other member

--- src/main/kotlin/api/epilogue/wehere/member/controller/MemberController.kt
+++ src/main/kotlin/api/epilogue/wehere/member/controller/MemberController.kt
@@ -4,10 +4,12 @@ import api.epilogue.wehere.auth.domain.MemberPrincipal
 import api.epilogue.wehere.member.application.MemberGetter
 import api.epilogue.wehere.member.application.MemberService
 import api.epilogue.wehere.member.application.UpdateMemberInput
+import java.util.UUID
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -21,6 +23,10 @@ class MemberController(
     @GetMapping("/me")
     fun getMe(@AuthenticationPrincipal principal: MemberPrincipal) =
         getter.get(principal.id)
+
+    @GetMapping("/{memberId}")
+    fun getOther(@PathVariable memberId: UUID) =
+        getter.get(memberId)
 
     @PatchMapping("/me")
     fun update(
